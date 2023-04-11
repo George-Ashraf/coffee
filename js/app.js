494aabe204e058c85a08a7c8921485da13f03374
George-Ashraf <georgeid20@gmail.com>
2023-04-12 01:18:33 +0200

    first commit

---
+++ js/app.js
@@ -0,0 +1,39 @@
+
+
+
+wow = new WOW(
+  {
+  boxClass:'animate__animated'
+
+}).init();
+// wow js
+
+  var hamburger = document.querySelector(".hamburger");
+  hamburger.addEventListener("click", function() {
+    hamburger.classList.toggle("is-active");
+  });
+  // hamburger
+
+  var swiper = new Swiper(".mySwiper", {
+    effect: "coverflow",
+    grabCursor: true,
+    centeredSlides: true,
+    slidesPerView: "auto",
+    autoplay: {
+      delay: 3000,
+      disableOnInteraction: false,
+    },
+    coverflowEffect: {
+      rotate: 50,
+      stretch: 0,
+      depth: 100,
+      modifier: 1,
+      slideShadows: true,
+    },
+    pagination: {
+      el: ".swiper-pagination",
+    },
+  });
+  // swiper
+  
+ 
