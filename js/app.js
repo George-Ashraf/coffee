e8714d508116c20912a57e8f38debca66fa425e6
George-Ashraf <georgeid20@gmail.com>
2023-05-29 20:03:40 +0300

    nav

--- js/app.js
+++ js/app.js
@@ -1,39 +1,33 @@
-
-
-
-wow = new WOW(
-  {
-  boxClass:'animate__animated'
+wow = new WOW({
+  boxClass: 'animate__animated'
 
 }).init();
 // wow js
 
-  var hamburger = document.querySelector(".hamburger");
-  hamburger.addEventListener("click", function() {
-    hamburger.classList.toggle("is-active");
-  });
-  // hamburger
+var hamburger = document.querySelector(".hamburger");
+hamburger.addEventListener("click", function () {
+  hamburger.classList.toggle("is-active");
+});
+// hamburger
 
-  var swiper = new Swiper(".mySwiper", {
-    effect: "coverflow",
-    grabCursor: true,
-    centeredSlides: true,
-    slidesPerView: "auto",
-    autoplay: {
-      delay: 3000,
-      disableOnInteraction: false,
-    },
-    coverflowEffect: {
-      rotate: 50,
-      stretch: 0,
-      depth: 100,
-      modifier: 1,
-      slideShadows: true,
-    },
-    pagination: {
-      el: ".swiper-pagination",
-    },
-  });
-  // swiper
-  
- 
+var swiper = new Swiper(".mySwiper", {
+  effect: "coverflow",
+  grabCursor: true,
+  centeredSlides: true,
+  slidesPerView: "auto",
+  autoplay: {
+    delay: 3000,
+    disableOnInteraction: false,
+  },
+  coverflowEffect: {
+    rotate: 50,
+    stretch: 0,
+    depth: 100,
+    modifier: 1,
+    slideShadows: true,
+  },
+  pagination: {
+    el: ".swiper-pagination",
+  },
+});
+// swiper
