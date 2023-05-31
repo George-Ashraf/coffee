9256b29ad5f37739db3f8c1542d077e37e846840
George-Ashraf <georgeid20@gmail.com>
2023-05-31 23:52:27 +0300

    sticky

--- js/app.js
+++ js/app.js
@@ -31,3 +31,9 @@ var swiper = new Swiper(".mySwiper", {
   },
 });
 // swiper
+
+// scroll
+window.addEventListener('scroll',function(){
+  let nav=document.querySelector('nav')
+  nav.classList.toggle('sticky',window.scrollY>0)
+})
